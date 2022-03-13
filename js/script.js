129d062249db62bca62c4512ec15a8e8a001f60f
Njoki <kinuthiash@gmail.com>
2022-03-14 00:13:26 +0300

    fix:portfolio-edit overlay styling

--- js/script.js
+++ js/script.js
@@ -5,45 +5,61 @@
 
 // what we do toggle UI CODE toggle
 // TODO:CLEAN UP - DRY
-$(document).ready(function(){
-  $("#design-show").click(function(){
+$(document).ready(function () {
+  $("#design-show").click(function () {
     $("#design-hidden").toggle();
     $("#design-show").toggle();
   })
-  $("#design-hidden").click(function(){
+  $("#design-hidden").click(function () {
     $("#design-show").toggle();
     $("#design-hidden").toggle();
   })
-  $("#dev-show").click(function(){
+  $("#dev-show").click(function () {
     $("#dev-hidden").toggle();
     $("#dev-show").toggle();
   })
-  $("#dev-hidden").click(function(){
+  $("#dev-hidden").click(function () {
     $("#dev-show").toggle();
     $("#dev-hidden").toggle();
   })
-  $("#mng-show").click(function(){
+  $("#mng-show").click(function () {
     $("#mng-hidden").toggle();
     $("#mng-show").toggle();
   })
-  $("#mng-hidden").click(function(){
+  $("#mng-hidden").click(function () {
     $("#mng-show").toggle();
     $("#mng-hidden").toggle();
   })
 })
 
 // tohover over portforlio
-
-// $(document).ready(function(){
-//   $(".hover-fadeout").hover(function(){
-//     $(".hover-fadeout").fadeTo("slow",0.5);
-//   });
-// });
-
+// TODO:take back the fadeout
+// TODO:individual pics
 $(document).ready(function (){
-  $(".fadeout").hover(function () {
-    $(".fadeout").fadeTo("fast", 0.5);    
-    $(".fadein").toggle();   
+  $(".fadeout").hover(function () {       
+    $(".fadein-overlay").toggle();   
   });
 });
 
+
+// pop up message
+// $(document).ready(function(){
+//   $("#feedback").submit(function(event){
+//     let name = $("#name").val();
+//     alert("works fine");
+//   });
+// });
+
+$(document).ready(function() {
+  $('input').keyup(function(event) {
+      if (event.which === "enter")
+      {
+          event.preventDefault();
+          $("#feedback").submit(function(event){
+            let name = $("#name").val();
+            alert("works fine");
+          });
+          
+      }
+  });
+});
