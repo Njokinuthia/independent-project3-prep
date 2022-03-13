85db4edb1312428a3bf61c06f2a91c74d4fd83ae
Njoki <kinuthiash@gmail.com>
2022-03-13 12:50:48 +0300

    feat:toggle what we do section

--- js/script.js
+++ js/script.js
@@ -2,3 +2,32 @@
 // let body = document.getElementById("body");
 //  body.style.backgroundColor ="red";
 
+
+// what we do toggle UI CODE toggle
+// TODO:CLEAN UP - DRY
+$(document).ready(function(){
+  $("#design-show").click(function(){
+    $("#design-hidden").toggle();
+    $("#design-show").toggle();
+  })
+  $("#design-hidden").click(function(){
+    $("#design-show").toggle();
+    $("#design-hidden").toggle();
+  })
+  $("#dev-show").click(function(){
+    $("#dev-hidden").toggle();
+    $("#dev-show").toggle();
+  })
+  $("#dev-hidden").click(function(){
+    $("#dev-show").toggle();
+    $("#dev-hidden").toggle();
+  })
+  $("#mng-show").click(function(){
+    $("#mng-hidden").toggle();
+    $("#mng-show").toggle();
+  })
+  $("#mng-hidden").click(function(){
+    $("#mng-show").toggle();
+    $("#mng-hidden").toggle();
+  })
+})
